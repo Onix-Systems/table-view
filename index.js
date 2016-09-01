798a213d8af5d01f27d6a98a67dee18a4f862626
Vladimir Perepelitsa <vladimirperepelitsa101@gmail.com>
2016-09-01 15:20:23 +0300

    backend

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var http = require('https');
 var fs = require('fs');
 
+var file = fs.createWriteStream("file.json");
 var request = http.get("https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5", function(response) {
-    console.log(response);
+    response.pipe(file);
 });
