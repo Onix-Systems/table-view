54134b91769360ba244df9a75bd31b732c093f15
Vladimir Perepelitsa <vladimirperepelitsa101@gmail.com>
2016-09-01 18:10:35 +0300

    back

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 var express = require("express");
 var app = express();
-var request = require('request');
+var req = require('request');
+var data = require('./data.json');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-    var answer = null;
-    request('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5', function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log(body);
-        }
-    });
-    response.send('<p>test</p>');
+    if (request.query.url) {
+        req.get(request.query.url, function (error, resp, body) {
+            if (!error && resp.statusCode == 200) {
+                response.send(body);
+            }
+        });
+    } else {
+        response.send(data);
+    }
 });
 
 var port = process.env.PORT || 5000;
