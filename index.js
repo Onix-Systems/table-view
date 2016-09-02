24c853c9481dae6998704dd3d747cc365f752858
Vladimir Perepelitsa <vladimirperepelitsa101@gmail.com>
2016-09-02 15:55:55 +0300

    DGFG-8

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var express = require("express");
 var app = express();
 var req = require('request');
 var data = require('./data.json');
-app.use(express.logger());
 
 app.get('/', function(request, response) {
     if (request.query.url) {
